de3596621d06539fb9caa5dcc6a9ecfa146b1e7c
Risky-Asyam <riskyasyam@gmail.com>
2023-10-19 20:10:12 +0700

    Create MenuLainnya.java

---
+++ app/src/main/java/com/example/rahmatantravel/MenuLainnya.java
@@ -0,0 +1,14 @@
+package com.example.rahmatantravel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuLainnya extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_lainnya);
+    }
+}
