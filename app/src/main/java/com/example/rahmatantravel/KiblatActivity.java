f87abf56f18f497001518c0c79278a13f3f26923
Risky-Asyam <riskyasyam@gmail.com>
2023-10-23 19:49:27 +0700

    Create KiblatActivity.java

---
+++ app/src/main/java/com/example/rahmatantravel/KiblatActivity.java
@@ -0,0 +1,14 @@
+package com.example.rahmatantravel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KiblatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kiblat);
+    }
+}
