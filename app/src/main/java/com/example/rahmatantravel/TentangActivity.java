b97b8e6bb3bc03a0cbd849b45afde302fd4c6dce
Risky-Asyam <riskyasyam@gmail.com>
2023-10-31 07:27:14 +0700

    update splash, about us

--- app/src/main/java/com/example/rahmatantravel/TentangActivity.java
+++ app/src/main/java/com/example/rahmatantravel/TentangActivity.java
@@ -1,8 +1,12 @@
 package com.example.rahmatantravel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class TentangActivity extends AppCompatActivity {
 
@@ -10,5 +14,45 @@ public class TentangActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tentang);
+
+        CardView maps = findViewById(R.id.maps);
+        maps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGoogleMaps();
+            }
+        });
+
+        CardView cardWa = findViewById(R.id.cardWa);
+
+        cardWa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWhatsapp();
+            }
+        });
+    }
+
+    private void openGoogleMaps(){
+        Uri gmmIntentUri = Uri.parse("https://maps.app.goo.gl/qKqw2AY8Ya1nTFCP8");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        if (mapIntent.resolveActivity(getPackageManager()) != null){
+            startActivity(mapIntent);
+        }
+        else {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+            startActivity(browserIntent);
+        }
+    }
+
+    private void openWhatsapp(){
+        Uri uri = Uri.parse("https://wa.me/+62895366960593");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
     }
 }
