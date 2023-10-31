4b585013d6fd39784686dde357bd271043134336
Risky-Asyam <riskyasyam@gmail.com>
2023-10-31 21:07:56 +0700

    Update About Us

--- app/src/main/java/com/example/rahmatantravel/TentangActivity.java
+++ app/src/main/java/com/example/rahmatantravel/TentangActivity.java
@@ -27,6 +27,8 @@ public class TentangActivity extends AppCompatActivity {
 //        });
 
         CardView cardWa = findViewById(R.id.cardWa);
+        CardView cardIg = findViewById(R.id.cardIg);
+        CardView cardTt = findViewById(R.id.cardTt);
 
         cardWa.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,10 +36,22 @@ public class TentangActivity extends AppCompatActivity {
                 openWhatsapp();
             }
         });
+        cardIg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openInstagram();
+            }
+        });
+        cardTt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openTiktok();
+            }
+        });
     }
 
     private void initializeUI(){
-        String iframe = "<iframe src=https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3949.0923910294287!2d113.6399914749853!3d-8.19344769183829!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd6940300000001%3A0x1f2a06ab12ddc149!2sRahmatan%20Tour%20%26%20Travel!5e0!3m2!1sid!2sid!4v1698732913155!5m2!1sid!2sid width=600 height=450 frameborder=0 style=border:0</iframe>";
+        String iframe = "<iframe src=https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d434549.40374533384!2d74.24349628287739!3d31.690830957117996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sPakistan+Lahore!5e0!3m2!1sen!2s!4v1395138949280 width=600 height=450 frameborder=0 style=border:0</iframe>";
         googleMapWebView = (WebView) findViewById(R.id.googlemap_webView);
         googleMapWebView.getSettings().setJavaScriptEnabled(true);
         googleMapWebView.loadData(iframe,"text/html","utf-8");
@@ -57,12 +71,42 @@ public class TentangActivity extends AppCompatActivity {
     }
 
     private void openWhatsapp(){
-        Uri uri = Uri.parse("https://wa.me/+62895366960593");
+        Uri uri = Uri.parse("https://wa.me/62895366960593");
         Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        intent.setPackage("com.whatsapp");
+
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        } else{
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(browserIntent);
+        }
+    }
+    private void openInstagram(){
+        Uri uri = Uri.parse("https://instagram.com/rahmatantourandtravel?igshid=NzZlODBkYWE4Ng==");
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setPackage("com.instagram.android");
 
         if (intent.resolveActivity(getPackageManager()) != null){
             startActivity(intent);
         }
+        else{
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(browserIntent);
+        }
+    }
 
+    private void openTiktok(){
+        Uri uri = Uri.parse("https://www.tiktok.com/@rahmatantourtravel?_t=8gyExpS5e5q&_r=1");
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setPackage("com.zhiliaoapp.musically");
+
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
+        else{
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW,uri);
+            startActivity(browserIntent);
+        }
     }
 }
